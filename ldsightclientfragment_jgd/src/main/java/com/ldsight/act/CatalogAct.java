922c6cec043c306aa787ede1b19b6f2f75f4a0fc
liu <602694077@qq.com>
2019-01-24 19:54:04 +0800

    2019年1月24日 19:53:51  更改CheckBox大小

--- ldsightclientfragment_jgd/src/main/java/com/ldsight/act/CatalogAct.java
+++ ldsightclientfragment_jgd/src/main/java/com/ldsight/act/CatalogAct.java
@@ -11,55 +11,63 @@ import com.example.ldsightclient_jgd.R;
 
 public class CatalogAct extends Activity implements OnClickListener {
 
-	private Button brighten;
-	private Button illumination;
-	private Button monitoring;
+    private Button brighten;
+    private Button illumination;
+    private Button monitoring;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
 
-		this.setContentView(R.layout.catalog_activity);
+        this.setContentView(R.layout.catalog_activity);
 
-		findViews();
+        findViews();
 
-		// 关闭照明功能
-		illumination.setBackgroundColor(0xFF999999);
+        int getin = getin();
 
 
-	}
+        // 关闭照明功能
+        illumination.setBackgroundColor(0xFF999999);
 
-	private void findViews() {
-		brighten = (Button) findViewById(R.id.brighten);
-		illumination = (Button) findViewById(R.id.illumination);
-		monitoring = (Button) findViewById(R.id.monitoring);
 
-		brighten.setOnClickListener(this);
-		illumination.setOnClickListener(this);
-		monitoring.setOnClickListener(this);
-	}
+    }
 
-	@Override
-	public void onClick(View v) {
+    private void findViews() {
+        brighten = (Button) findViewById(R.id.brighten);
+        illumination = (Button) findViewById(R.id.illumination);
+        monitoring = (Button) findViewById(R.id.monitoring);
 
-		if (v == illumination) {  // 照明
+        brighten.setOnClickListener(this);
+        illumination.setOnClickListener(this);
+        monitoring.setOnClickListener(this);
+    }
 
-			Intent intent = new Intent(CatalogAct.this, ParameterAct.class);
-			intent.putExtra(ParameterAct.FRAGMENT_FLAG, ParameterAct.MAIN);
-			startActivity(intent);
+    @Override
+    public void onClick(View v) {
 
-		} else if (v == brighten) { // 亮化
+        if (v == illumination) {  // 照明
 
-			Intent intent = new Intent(CatalogAct.this, BrightenMain.class);
-			startActivity(intent);
+            Intent intent = new Intent(CatalogAct.this, ParameterAct.class);
+            intent.putExtra(ParameterAct.FRAGMENT_FLAG, ParameterAct.MAIN);
+            startActivity(intent);
 
-		} else if (v == monitoring) {
+        } else if (v == brighten) { // 亮化
 
-			Intent intent = new Intent(CatalogAct.this, MonitorMainAct.class);
-			startActivity(intent); // 监控
+            Intent intent = new Intent(CatalogAct.this, BrightenMain.class);
+            startActivity(intent);
 
-		}
-	}
+        } else if (v == monitoring) {
 
+            Intent intent = new Intent(CatalogAct.this, MonitorMainAct.class);
+            startActivity(intent); // 监控
+
+        }
+    }
+
+
+    private int getin() {
+
+        return 454;
+    }
 }
